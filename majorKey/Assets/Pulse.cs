c549dd0c6996336226839687c6e37aac58e57284
WildMangoProductions <just4kas@gmail.com>
2019-01-18 22:03:20 -0500

    MidiJack Integration
    Using free, open-sourced MidiJack, the keyboard now communicates to Unity and can control objects with scripts and key presses.

---
+++ majorKey/Assets/Pulse.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using MidiJack;
+
+public class Pulse : MonoBehaviour {
+
+    public int noteNumber;
+
+    void Update()
+    {
+
+        for (var i = 0; i < 128; i++)
+        {
+            if (MidiMaster.GetKeyDown(i))
+            {
+                print(MidiMaster.GetKeyDown(i));
+                var color = MidiMaster.GetKeyDown(i) ? Color.red : Color.white;
+                GetComponent<Renderer>().material.color = color;
+                transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(i));
+                print(i);
+            }
+        }
+
+    }
+
+
+}
