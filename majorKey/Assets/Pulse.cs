eab359177c266d4e880adeed4d468fe71201682e
WildMangoProductions <just4kas@gmail.com>
2019-01-20 03:23:31 -0500

    Notes Have Script
    Notes have script. They play all sounds tho.

--- majorKey/Assets/Pulse.cs
+++ majorKey/Assets/Pulse.cs
@@ -6,27 +6,46 @@ public class Pulse : MonoBehaviour {
     public int noteNumber;
     public AudioSource meow;
     public int transpose = -4;
+    public int midiNote;
     void Update()
     {
 
-        for (var i = 0; i < 128; i++)
+        if (MidiMaster.GetKeyDown(midiNote))
         {
-            if (MidiMaster.GetKeyDown(i))
-            {
-                print(MidiMaster.GetKeyDown(i));
-                var color = Color.yellow;
-                GetComponent<Renderer>().material.color = color;
-                //transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(i));
-                print(i);
-                meow.Play();
+            print(MidiMaster.GetKeyDown(midiNote));
+            var color = Color.yellow;
+            GetComponent<Renderer>().material.color = color;
+            //transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(i));
+            print(midiNote);
+            meow.Play();
 
-            }
-            if (MidiMaster.GetKeyUp(i))
-            {
-                var color = Color.white;
-                GetComponent<Renderer>().material.color = color;
-                //transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(i));
-            }
         }
+        if (MidiMaster.GetKeyUp(midiNote))
+        {
+            var color = Color.white;
+            GetComponent<Renderer>().material.color = color;
+            //transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(i));
+        }
+
+        //for (var i = 0; i < 128; i++)
+        //{
+        //    if (MidiMaster.GetKeyDown(midiNote))
+        //    {
+        //        print(MidiMaster.GetKeyDown(i));
+        //        var color = Color.yellow;
+        //        GetComponent<Renderer>().material.color = color;
+        //        //transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(i));
+        //        print(i);
+        //        meow.Play();
+
+        //    }
+        //    if (MidiMaster.GetKeyUp(i))
+        //    {
+        //        var color = Color.white;
+        //        GetComponent<Renderer>().material.color = color;
+        //        //transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(i));
+        //    }
+        //}
+
     }
 }
