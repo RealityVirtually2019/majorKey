5a6da60daa84992d09577bee25694370e9361c21
WildMangoProductions <just4kas@gmail.com>
2019-01-20 12:05:50 -0500

    PIANO CONTROLLER WORKS!!!
    You can drag the digital piano over the real piano and it's a perfect 1 to 1 fit.

--- majorKey/Assets/ControlScript.cs
+++ majorKey/Assets/ControlScript.cs
@@ -14,6 +14,11 @@ public class ControlScript : MonoBehaviour
     private const float _moveSpeed = 1.2f;
     private bool _enabled = false;
     private bool _bumper = false;
+    private bool _isGrabbing;
+    private GameObject obj;
+
+    [SerializeField]
+    GameObject tip;
 
     void Awake()
     {
@@ -21,75 +26,45 @@ public class ControlScript : MonoBehaviour
         Console.WriteLine("Hello World!");
         _cube = GameObject.Find("Cube");
         MLInput.Start();
-        MLInput.OnControllerButtonDown += OnButtonDown;
-        MLInput.OnControllerButtonUp += OnButtonUp;
+
         _controller = MLInput.GetController(MLInput.Hand.Left);
+        _isGrabbing = false;
     }
 
     void OnDestroy()
     {
-        MLInput.OnControllerButtonDown -= OnButtonDown;
-        MLInput.OnControllerButtonUp -= OnButtonUp;
+
         MLInput.Stop();
     }
 
     void Update()
     {
         transform.position = _controller.Position;
+        transform.rotation= _controller.Orientation;
         //print(_controller.Position);
-        if (_bumper && _enabled)
-        {
-            _cube.GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.Range(0, 255), UnityEngine.Random.Range(0, 255), UnityEngine.Random.Range(0, 255));
-        }
-        CheckControl();
-    }
 
-    void CheckControl()
-    {
-        if (_controller.TriggerValue > 0.2f && _enabled)
-        {
-            _bumper = false;
-            _cube.transform.Rotate(Vector3.up, -_rotationSpeed * Time.deltaTime);
-            _cube.transform.position = _controller.Position;
-        }
-        else if (_controller.Touch1PosAndForce.z > 0.0f && _enabled)
-        {
-            //float X = _controller.Touch1PosAndForce.x; TOUCHPAD
-            //float Y = _controller.Touch1PosAndForce.y;
-            //Vector3 forward = Vector3.Normalize(Vector3.ProjectOnPlane(transform.forward, Vector3.up));
-            //Vector3 right = Vector3.Normalize(Vector3.ProjectOnPlane(transform.right, Vector3.up));
-            //Vector3 force = Vector3.Normalize((X * right) + (Y * forward));
-            //_cube.transform.position += force * Time.deltaTime * _moveSpeed;
+        if (_isGrabbing) { 
+        obj.transform.position = tip.transform.position;
+            obj.transform.rotation = tip.transform.rotation;
         }
     }
 
-    void OnButtonDown(byte controller_id, MLInputControllerButton button)
-    {
-        if ((button == MLInputControllerButton.Bumper && _enabled))
-        {
-            _bumper = true;
-        }
-    }
-
-    void OnButtonUp(byte controller_id, MLInputControllerButton button)
-    {
-        if (button == MLInputControllerButton.HomeTap)
-        {
-            _cube.SetActive(true);
-            _cube.transform.position = transform.position + transform.forward * _distance;
-            _cube.transform.rotation = transform.rotation;
-            _enabled = true;
-        }
-    }
 
     private void OnTriggerStay(Collider other)
     {
-        //print(other.gameObject.name);
+        print(other.name);
+        obj = other.gameObject;
+        print(other.gameObject.name);
         if (_controller.TriggerValue > 0.8f)
         {
-            other.gameObject.transform.position = _controller.Position;
+            _isGrabbing = true;
         }
-
+        else
+        {
+            _isGrabbing = false;
+        }
     }
+
+
 
 }
